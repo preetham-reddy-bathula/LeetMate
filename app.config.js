38c528ab4dde4c76d28b52779733a57d27097925
Preetham Kumar Reddy Bathula <65440305+preetham-reddy-bathula@users.noreply.github.com>
2024-07-14 01:01:46 +0530

    setup firebase auth

---
+++ app.config.js
@@ -0,0 +1,18 @@
+export default {
+    expo: {
+      name: "LeetMate",
+      slug: "leetmate",
+      version: "1.0.0",
+      sdkVersion: "42.0.0",
+      platforms: ["ios", "android"],
+      extra: {
+        firebaseApiKey: process.env.FIREBASE_API_KEY,
+        firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+        firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        firebaseAppId: process.env.FIREBASE_APP_ID
+      }
+    }
+  };
+
---
+++ services/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.firebaseApiKey,
+  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+  appId: Constants.expoConfig.extra.firebaseAppId
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
